1fcf99b5edc9e9444264a731b06f87f5f4d95aa0
hidenao-otake <h0i6d2e1@gmail.com>
2023-03-02 08:43:13 +0900

    Learn Pick<T, K>

---
+++ src/Pick/index.ts
@@ -0,0 +1,44 @@
+export default function pickSample() {
+  // 型<T>から任意のキーを含むオブジェクト型を返す
+  type User = {
+    surname: string
+    middleName?: string
+    givenName: string
+    age: number
+    address?: string
+    nationality: string
+    createdAt: string
+    updatedAt: string
+  }
+  type Person = Pick<User, 'surname' | 'middleName' | 'givenName'>
+  // これと同義となる
+  // type Person = {
+  //   surname: string;
+  //   middleName?: string;
+  //   givenName: string;
+  // };
+
+  // 型の変更に柔軟に追従する例
+  // 書籍の検索サービスなどで、こんなBook型があるとして
+  type Book = {
+    id: number
+    title: string
+    author: string
+    createdAt: Date
+    updatedAt: Date
+  }
+
+  // Book 自体を作成する際に求められるのは、
+  // title と author のみという状況（他の情報は後付け）だとこうなる
+  type BookInputData = {
+    title: string
+    author: string
+  }
+
+  // しかし、プロジェクトの方針で author に対して、
+  // Person型 を割り当てることになったら、修正が面倒
+
+  // これを回避するために BookInputType 型は、
+  // Pick<T, K> を使って型を引用
+  type IBookInputData = Pick<Book, 'title' | 'author'>
+}
